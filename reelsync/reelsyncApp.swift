7864018e9ee83f923263d0cda553c5ba7614a21b
Mirko314 <mirko.budszuhn@gmail.com>
2021-11-12 12:07:43 +0100

    Initial Commit

---
+++ reelsync/reelsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  reelsyncApp.swift
+//  reelsync
+//
+//  Created by Mirko Budszuhn on 12.11.21.
+//
+
+import SwiftUI
+
+@main
+struct reelsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
